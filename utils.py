ff04a03a3102051a0bc74edc9103836367b27609
Jana Pr <jana@alterolab.com>
2025-08-03 18:16:28 +0800

    feat: update model

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 import io
 
-model = YOLO("model/best.pt")
+model = YOLO("model/model_v1.pt")
 
 def detect_objects(image_bytes: bytes):
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
